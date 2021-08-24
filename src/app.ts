75791220a8408f2829c56438b9ab84625f35e1cd
TheAsdas <gustavoherrera1997@gmail.com>
2021-08-24 15:04:14 -0400

    rutas agregadas y cors instalado}

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import dotenv from "dotenv";
-import Server from './models/Server';
+import Server from "./models/Server";
 
 dotenv.config();
 
-Server.init();
-Server.listen();
-
-
-
-
-
-
+Server.init().listen();
--- src/models/Server.ts
+++ src/models/Server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import * as core from "express-serve-static-core";
+import cors from "cors";
 
 class Server {
   private static _app: core.Express;
@@ -13,24 +14,40 @@ class Server {
     //dotenv.config();
     this._app = express();
     this._port = process.env.PORT;
-    this.middlewares();
-    this.routes();
+    this.middlewares().routes();
+
+    return this;
   }
 
   private static routes() {
     this._app.get("/api", (req, res) => {
-      res.send("Hello, world.");
+      res.status(418).json({ status: "get", response: "Hola, mundo!" });
     });
+    this._app.put("/api", (req, res) => {
+      res.json({ status: "put", response: "Hola, mundo!" });
+    });
+    this._app.post("/api", (req, res) => {
+      res.status(201).json({ status: "post", response: "Hola, mundo!" });
+    });
+    this._app.delete("/api", (req, res) => {
+      res.json({ status: "delete", response: "Hola, mundo!" });
+    });
+
+    return this;
   }
 
   private static middlewares() {
-    this._app.use(express.static("dist/public"));
+    this._app.use(express.static("dist/public"), cors());
+
+    return this;
   }
 
   static listen() {
     this._app.listen(this._port, () => {
       console.log("Escuchando en el puerto " + this._port);
     });
+
+    return this;
   }
 }
 
